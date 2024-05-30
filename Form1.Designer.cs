9970c2f11dab0bdecb694e4c01d6d250e6ba7823
SHADOW <100056807+shadow-nero@users.noreply.github.com>
2024-05-30 18:02:55 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace AlterAFS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.bt_ExtractAFS = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(813, 467);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackgroundImage = global::AlterAFS.Properties.Resources.background;
+            this.tabPage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(805, 441);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Fate/UC (PS2)";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackgroundImage = global::AlterAFS.Properties.Resources.background;
+            this.tabPage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(805, 441);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Fate/UC (PSP)";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.Gray;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DATA ASSETS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.bt_ExtractAFS);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 427);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // bt_ExtractAFS
+            // 
+            this.bt_ExtractAFS.BackColor = System.Drawing.Color.Black;
+            this.bt_ExtractAFS.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.bt_ExtractAFS.Location = new System.Drawing.Point(6, 37);
+            this.bt_ExtractAFS.Name = "bt_ExtractAFS";
+            this.bt_ExtractAFS.Size = new System.Drawing.Size(188, 44);
+            this.bt_ExtractAFS.TabIndex = 2;
+            this.bt_ExtractAFS.Text = "EXTRACT \"AFS\"";
+            this.bt_ExtractAFS.UseVisualStyleBackColor = false;
+            this.bt_ExtractAFS.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(6, 87);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "REPACK \"AFS\"";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 467);
+            this.Controls.Add(this.tabControl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "AlterAFS";
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button bt_ExtractAFS;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlterAFS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            using(OpenFileDialog AFS = new OpenFileDialog())
+            {
+                AFS.Title = "Select AFS (PS2)";
+                AFS.Filter = "AFS Files (*.afs)|*.afs|All Files (*.*)|*.*";
+                AFS.FileName = "Data.afs";
+
+                using(OpenFileDialog EBOOT = new OpenFileDialog())
+                {
+                    EBOOT.Title = "Select SLPM_551.08 (PS2)";
+                    EBOOT.Filter = "SLPM_551.08 (*.08)|*.08|All Files (*.*)|*.*";
+                    EBOOT.FileName = "SLPM_551.08";
+
+                    if (AFS.ShowDialog() == DialogResult.OK && EBOOT.ShowDialog() == DialogResult.OK)
+                    {
+
+
+                        string Dest = "Fate UC (PS2)\\EXTRACTED\\" + Path.GetFileNameWithoutExtension(AFS.FileName).ToLower();
+
+                        if (Directory.Exists(Dest) == false)
+                        {
+                            Directory.CreateDirectory(Dest);
+                        }
+
+                        string[] Paths = EbootPath.DecompileEboot(EBOOT.FileName, AFS.FileName.ToLower());
+
+                        AFSUnpacker.AFSExtract(AFS.FileName, Dest, Paths);
+                        MessageBox.Show("Done!", "Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    //AFSPacker.AFSRepack("BGM", "BGM01.AFS", Paths);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog EBOOT = new OpenFileDialog())
+            {
+                EBOOT.Title = "Select SLPM_551.08 (PS2)";
+                EBOOT.Filter = "SLPM_551.08 (*.08)|*.08|All Files (*.*)|*.*";
+                EBOOT.FileName = "SLPM_551.08";
+
+                if (EBOOT.ShowDialog() == DialogResult.OK)
+                {
+                    string Dest = "Fate UC (PS2)\\REPACK\\";
+                    if (Directory.Exists(Dest) == false)
+                    {
+                        Directory.CreateDirectory(Dest);
+                    }
+
+                    foreach (string afsRepack in Directory.GetDirectories("Fate UC (PS2)\\EXTRACTED")) {
+
+
+                        string[] Paths = EbootPath.DecompileEboot(EBOOT.FileName, Path.GetFileNameWithoutExtension(afsRepack) + ".afs");
+                        //MessageBox.Show(Path.GetFileNameWithoutExtension(afsRepack), "Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        
+                        AFSPacker.AFSRepack(afsRepack, Dest + Path.GetFileNameWithoutExtension(afsRepack) + ".afs", Paths);
+                    }
+                    MessageBox.Show("Done!", "Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                //AFSPacker.AFSRepack("BGM", "BGM01.AFS", Paths);
+            }
+        }
+    }
+}
